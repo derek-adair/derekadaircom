421a0e88cec14fe95c88d4830434385c58807de4
Derek <d@derekadair.com>
2012-07-19 07:52:36 -0500

    Large overdue commit - added pages.  Added jquery UI w/o theme.  Added fancy menu animation and image

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 /* Author: Derek Adair
 
 */
+$(document).ready(function(){ 
+    //menu animation TODO: work out xbrowser
+    $('nav a').hover(
+         function(){
+            $(this).stop().animate({
+                'background-position-y': '0%',
+                color: "#06799F"
+            }, 1000);
+            return false;
+        },
+        function(){
+            $(this).stop().animate({
+                'background-position-y': '100%',
+                color: "white"
+        }, 1000);
+    });
 
-
-
-
-
+    $("nav a").pjax('#main', {url: $(this).attr("href"), push: false});
+    $('#main').live('pjax:error', function(e, xhr, err) {
+         alert('Something went wrong: ' + err)
+    })
+});
