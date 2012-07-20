bfbdff99a3d120f27e0af856525f43109d3ea798
Derek <d@derekadair.com>
2012-07-20 04:08:56 -0500

    massive commit... major content updates | PRE INDEX REPLACE<<<<<<<<<<

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
 /* Author: Derek Adair
 
 */
+var blah = 'blah';
 $(document).ready(function(){ 
     //menu animation TODO: work out xbrowser
     $('nav a').hover(
-         function(){
-            $(this).stop().animate({
-                'background-position-y': '0%',
-                color: "#06799F"
-            }, 1000);
-            return false;
-        },
         function(){
-            $(this).stop().animate({
-                'background-position-y': '100%',
-                color: "white"
+        $(this).stop().animate({
+            'background-position-y': '0%',
+            color: "#06799F"
+        }, 1000);
+        return false;
+    },
+    function(){
+        $(this).stop().animate({
+            'background-position-y': '100%',
+            color: "white"
         }, 1000);
     });
 
-    $("nav a").pjax('#main', {url: $(this).attr("href"), push: false});
+    $("a").pjax('#main', {url: $(this).attr("href"), fragment:"#main"});
     $('#main').live('pjax:error', function(e, xhr, err) {
-         alert('Something went wrong: ' + err)
+        alert('Something went wrong: ' + err)
     })
 });
