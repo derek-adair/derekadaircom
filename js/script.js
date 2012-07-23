f3d2295a408627f6604734fb201682e128b1f7c8
Derek <d@derekadair.com>
2012-07-22 21:33:31 -0500

    sitemap.xml, contact form

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
 /* Author: Derek Adair
 
 */
-var blah = 'blah';
 $(document).ready(function(){ 
+
+    // HACK HACK HACK HACK HACK HACK HAK HACK HACK HAKHAKHAKHAKHA
+    $('a[href="http://www.foxyform.com"]').css('display', 'none');
+    // end hackery
+
     //menu animation TODO: work out xbrowser
+    setTimeout(function(){$("#title").show('clip', {percent: 100}, 500);}, 1000);
     $('nav a').hover(
         function(){
-        $(this).stop().animate({
-            'background-position-y': '0%',
-            color: "#06799F"
-        }, 1000);
-        return false;
-    },
-    function(){
-        $(this).stop().animate({
-            'background-position-y': '100%',
+            $(this).stop().animate({
+                'background-position-y': '0%',
+                color: "#06799F"
+            }, 1000);
+            return false;
+        },
+        function(){
+            $(this).stop().animate({
+                'background-position-y': '100%',
             color: "white"
-        }, 1000);
+            }, 1000);
+        });
+
+    $("a").pjax('#main', {
+        url: $(this).attr("href"), 
+        fragment:"#main"
     });
 
-    $("a").pjax('#main', {url: $(this).attr("href"), fragment:"#main"});
-    $('#main').live('pjax:error', function(e, xhr, err) {
-        alert('Something went wrong: ' + err)
-    })
+    $("#main").on('pjax:end',   function() { 
+        // HACK HACK HACK HACK HACK HACK HAK HACK HACK HAKHAKHAKHAKHA
+        $('a[href="http://www.foxyform.com"]').css('display', 'none');
+        console.log('blah...');
+    });
 });
