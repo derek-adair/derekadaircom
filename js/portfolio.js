2fd4a4434a3d620991c7fcaa5e846040758be50d
Derek Adair <d@derekadair.com>
2012-09-01 14:32:28 -0500

    stable CSS friendly portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
     $(".portfolio-gallery").minimalGallery();
+    /*$("section").css("height", $(window).height());
+    $("h2.divider").css("margin-bottom", $(window).height());*/
 });
